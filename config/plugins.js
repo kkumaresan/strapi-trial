bc7b293062a77c9fcc240f0ba78570f63c68c644
K Kumaresan <ux.kumaresan@gmail.com>
2023-07-30 20:47:04 +0530

    Slugify fix

--- config/plugins.js
+++ config/plugins.js
@@ -2,10 +2,10 @@ module.exports = ({ env }) => ({
   slugify: {
     enabled: true,
     config: {
-      contentTypesmodelName: {
-        deals: { //write what your collection type's name is this case we should use "blog"
-          field: "slug",
-          references: "slug",
+      contentTypes: {
+        deal: {
+          field: 'slug',
+          references: 'title',
         },
       },
     },
